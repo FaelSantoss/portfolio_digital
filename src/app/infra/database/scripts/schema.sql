9e5fbe2d179e804715e290fbe634ceb9ebd778ab
FaelSantoss <silva.faelm@gmail.com>
2024-05-31 17:15:27 -0300

    refactor database

--- src/app/infra/database/scripts/schema.sql
+++ src/app/infra/database/scripts/schema.sql
@@ -1,15 +1,21 @@
+-- Active: 1716055461921@@127.0.0.1@3306@bla
 CREATE DATABASE IF NOT EXISTS `bla`;
-
 CREATE TABLE IF NOT EXISTS personal_project (
     id CHAR(36) DEFAULT(UUID()) PRIMARY KEY NOT NULL,
     title VARCHAR(255),
-    desc VARCHAR(255),
+    content VARCHAR(255),
     technologies VARCHAR(255)
 );
 
 CREATE TABLE IF NOT EXISTS academic_project (
     id CHAR(36) DEFAULT(UUID()) PRIMARY KEY NOT NULL,
     title VARCHAR(255),
-    desc VARCHAR(255),
+    content VARCHAR(255),
     technologies VARCHAR(255)
 );
+
+INSERT INTO personal_project (title, content, technologies)
+VALUES ('Projeto Exemplo 1', 'Este é o conteúdo do projeto exemplo 1.', 'HTML, CSS, JavaScript');
+
+INSERT INTO personal_project (title, content, technologies)
+VALUES ('Projeto Exemplo 2', 'Este é o conteúdo do projeto exemplo 2.', 'Python, Django');
