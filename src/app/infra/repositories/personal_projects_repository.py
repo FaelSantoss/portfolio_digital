7696f465dcfc72a65533f513b5086247c26eacb9
FaelSantoss <silva.faelm@gmail.com>
2024-06-01 01:03:49 -0300

    refactor PersonalProjectsRepository

--- src/app/infra/repositories/personal_projects_repository.py
+++ src/app/infra/repositories/personal_projects_repository.py
@@ -3,13 +3,13 @@ from infra.database import mysql
 
 class PersonalProjectsRepository:
     def get_personal_projects(self) -> list[PersonalProject]:
-        print('aaaaaaaaaaaaaaaaaaaaaaaaaaaaa', flush=True)
         select_query = "SELECT * FROM personal_project"
         rows = mysql.query(sql=select_query, is_single=False)
 
         personal_projects = []
 
         for row in rows:
-            personal_projects.append(PersonalProject(id=row["id"], title=row["title"], content=row["content"], technologies=row["technologies"]))
+            personal_projects.append(PersonalProject(id=row["id"], title=row["title"], content=row["content"], technologies=row["technologies"], link=row["link"]))
 
         return personal_projects
+    
