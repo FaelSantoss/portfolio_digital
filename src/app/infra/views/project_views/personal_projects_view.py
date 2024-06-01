6ee36c02aadd0b70aa46fc9157054aca33abbb82
FaelSantoss <silva.faelm@gmail.com>
2024-06-01 01:04:59 -0300

    create personal_projects_view

---
+++ src/app/infra/views/project_views/personal_projects_view.py
@@ -0,0 +1,11 @@
+from flask import render_template
+
+from infra.repositories import personal_projects_repository
+
+def personal_projects_view():
+    personal_projects = personal_projects_repository.get_personal_projects()
+    print(personal_projects, flush=True)
+    return render_template(
+        "projects/personal_projects.html",
+        personal_projects = personal_projects
+    )
