4b415e003e4d2161f9d30c394671b145698f630d
FaelSantoss <silva.faelm@gmail.com>
2024-06-01 01:03:13 -0300

    insert projects

--- src/app/infra/database/scripts/schema.sql
+++ src/app/infra/database/scripts/schema.sql
@@ -1,21 +1,33 @@
--- Active: 1716055461921@@127.0.0.1@3306@bla
+-- Active: 1716055461921@@127.0.0.1@3306@fael
 CREATE DATABASE IF NOT EXISTS `bla`;
 CREATE TABLE IF NOT EXISTS personal_project (
     id CHAR(36) DEFAULT(UUID()) PRIMARY KEY NOT NULL,
     title VARCHAR(255),
     content VARCHAR(255),
-    technologies VARCHAR(255)
+    technologies VARCHAR(255),
+    link VARCHAR(255)
 );
 
 CREATE TABLE IF NOT EXISTS academic_project (
     id CHAR(36) DEFAULT(UUID()) PRIMARY KEY NOT NULL,
     title VARCHAR(255),
     content VARCHAR(255),
-    technologies VARCHAR(255)
+    technologies VARCHAR(255),
+    link VARCHAR(255)
 );
 
-INSERT INTO personal_project (title, content, technologies)
-VALUES ('Projeto Exemplo 1', 'Este é o conteúdo do projeto exemplo 1.', 'HTML, CSS, JavaScript');
+INSERT INTO personal_project (title, content, technologies, link)
+VALUES ('Projeto Todo List',
+        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit distinctio vero dolorum ipsa quod maxime, minus, earum doloribus laborum nobis unde aliquam sequi incidunt natus beatae dolor debitis iure eius!',
+         'HTML, CSS, JavaScript',
+         'https://github.com/FaelSantoss/projeto-todo'
+         );
 
-INSERT INTO personal_project (title, content, technologies)
-VALUES ('Projeto Exemplo 2', 'Este é o conteúdo do projeto exemplo 2.', 'Python, Django');
+INSERT INTO personal_project (title, content, technologies, link)
+VALUES ('ZipCode Finder',
+        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit distinctio vero dolorum ipsa quod maxime, minus, earum doloribus laborum nobis unde aliquam sequi incidunt natus beatae dolor debitis iure eius!',
+         'HTML, CSS, JavaScript, React',
+         'https://github.com/FaelSantoss/zip-code-finder'
+         );
+
+SELECT * FROM personal_project;
