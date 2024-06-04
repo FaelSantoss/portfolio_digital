f2fcc3986f8b35bc145262681698a96cd92531f4
FaelSantoss <silva.faelm@gmail.com>
2024-06-04 11:52:54 -0300

    added animation in homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        fadeIn: 'fadeIn 1s ease-in-out',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+      },
+    },
   },
+  variants: {},
   plugins: [],
-}
-
+};
