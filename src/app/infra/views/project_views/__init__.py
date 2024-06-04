2d01844af8937c4cbc6edbc6be0fa075fd8f926c
FaelSantoss <silva.faelm@gmail.com>
2024-06-04 10:22:07 -0300

    update navbar

--- src/app/infra/views/project_views/__init__.py
+++ src/app/infra/views/project_views/__init__.py
@@ -1,22 +1,26 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 projects_views = Blueprint("projects_views", __name__)
 
 from .personal_projects_view import personal_projects_view
 
+
 @projects_views.route("/", methods=["GET"])
 def homepage():
-    return render_template("index.html")
+    url = str(request.path)
+    return render_template("index.html", url=url)
 
 
 @projects_views.route("/about", methods=["GET"])
 def about():
-    return render_template("about.html")
+    url = str(request.path)
+    return render_template("about.html", url=url)
 
 
 @projects_views.route("/projects", methods=["GET"])
 def projects():
-    return render_template("projects/index.html")
+    url = str(request.path)
+    return render_template("projects/index.html", url=url)
 
 
 @projects_views.route("/projects/academic", methods=["GET"])
--- src/app/infra/views/project_views/personal_projects_view.py
+++ src/app/infra/views/project_views/personal_projects_view.py
@@ -1,10 +1,11 @@
-from flask import render_template
+from flask import render_template, request
 
 from infra.repositories import personal_projects_repository
 
+
 def personal_projects_view():
+    url = str(request.path)
     personal_projects = personal_projects_repository.get_personal_projects()
     return render_template(
-        "projects/personal_projects.html",
-        personal_projects = personal_projects
+        "projects/personal_projects.html", personal_projects=personal_projects, url=url
     )
