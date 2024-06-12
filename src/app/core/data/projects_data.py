b069a6566b59a73240887c61a030d432e69ff226
FaelSantoss <silva.faelm@gmail.com>
2024-06-12 15:05:06 -0300

    added content

--- src/app/core/data/projects_data.py
+++ src/app/core/data/projects_data.py
@@ -1,32 +1,28 @@
 projects_data = [
   {
-    "id": "472656b0-27a0-11ef-b24d-0242c0a82002",
     "title": "Smart-Farming",
-    "content": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit distinctio vero dolorum ipsa quod maxime, minus, earum doloribus laborum nobis unde aliquam sequi incidunt natus beatae dolor debitis iure eius!",
+    "content": "Desenvolvemos uma aplicação web para o monitoramento remoto e análise de dados de uma estufa na faculdade. Minhas contribuições incluíram o desenvolvimento do backend, criação de repositórios (CRUD), entidades, exportação de dados para .xlsx e participação significativa na modelagem do banco de dados.",
     "technologies": "javascript,python,flask,html5,css3,tailwindcss,docker,mysql",
     "link": "https://github.com/FaelSantoss/zip-code-finder",
     "img": "smartfarming.png"
   },
   {
-    "id": "49a9e6a4-27a0-11ef-b24d-0242c0a82002",
     "title": "Meu Timão",
-    "content": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit distinctio vero dolorum ipsa quod maxime, minus, earum doloribus laborum nobis unde aliquam sequi incidunt natus beatae dolor debitis iure eius!",
+    "content": "Desenvolvi o site 'Meu Timão', onde os usuários podem adicionar notícias que são exibidas em forma de cards. Cada card possui um botão que gera uma rota dinâmica para uma página específica no banco de dados. A interface inclui um carrossel com as três últimas notícias, utilizando componentes do Bootstrap.",
     "technologies": "python,flask,html5,css3,bootstrap,docker,mysql",
     "link": "https://github.com/FaelSantoss/zip-code-finder",
     "img": "meutimao.png"
   },
   {
-    "id": "b0caed5f-276d-11ef-a3e7-0242c0a82002",
     "title": "Todo List",
-    "content": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit distinctio vero dolorum ipsa quod maxime, minus, earum doloribus laborum nobis unde aliquam sequi incidunt natus beatae dolor debitis iure eius!",
+    "content": "Desenvolvi o 'Todo Avançado', uma aplicação para adicionar, pesquisar, filtrar, editar e excluir tarefas.",
     "technologies": "javascript,html5,css3",
     "link": "https://github.com/FaelSantoss/projeto-todo",
     "img": "todolist.png"
   },
   {
-    "id": "b18b0f72-276d-11ef-a3e7-0242c0a82002",
-    "title": "ZipCode Finder",
-    "content": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit distinctio vero dolorum ipsa quod maxime, minus, earum doloribus laborum nobis unde aliquam sequi incidunt natus beatae dolor debitis iure eius!",
+    "title": "Buscador CEP",
+    "content": "Desenvolvi o 'Buscador CEP', uma aplicação que utiliza uma API externa para buscar informações de endereço com base no CEP inserido pelo usuário.",
     "technologies": "javascript,react,html5,css3",
     "link": "https://github.com/FaelSantoss/zip-code-finder",
     "img": "zipcodefinder.png"
